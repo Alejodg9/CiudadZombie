b92f91073bd5c4a10269113c70ee8b8f37b9abe5
Alejo <digiovine.a@gmail.com>
2019-12-03 01:19:10 -0300

    jugador se mueve y pierde vidas

--- js/jugador.js
+++ js/jugador.js
@@ -10,7 +10,19 @@ var Jugador = {
   alto: 30,
   velocidad: 10,
   vidas: 5,
+
   // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
   // y todo lo que haga falta para que cumpla con sus responsabilidades
+
+  mover: function (movX, movY, spriteActual) {
+    this.x += movX;
+    this.y += movY;
+    this.sprite = spriteActual;
+
+  },
+
+  perderVidas: function (cantVidas) {
+    this.vidas -= cantVidas;
+   }
 
 }
